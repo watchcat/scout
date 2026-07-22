cd9d2040cc266bbbf8337cce09f1db3182f00362
watchcat <watchcat@gmail.com>
2026-07-22 17:02:35 +0200

    feat: scaffold scout crate with dependencies

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Modules are added task by task; dead_code is allowed until final wiring
+// (removed in the last task).
+#![allow(dead_code)]
+
+fn main() {
+    println!("scout: not wired up yet");
+}
